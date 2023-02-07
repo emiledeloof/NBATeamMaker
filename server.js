7c0bc9dd16ef06fcd429041d51ac9a917eb70134
Emile Deloof <deloofemile@gmail.com>
2023-02-07 18:39:17 +0100

    minor ux changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const router = require("./routes/router")
 const app = express()
-const PORT = 5001
+const PORT = process.env.PORT || 5001
 
 mongoose.connect("mongodb+srv://admin:admin@cluster0.licu4m5.mongodb.net/?retryWrites=true&w=majority")
 
