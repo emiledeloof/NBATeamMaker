0eff2e09c46f9bedfe6470f0ddd0bc129c7ec74f
Emile Deloof <deloofemile@gmail.com>
2023-02-10 20:18:05 +0100

    group database

---
+++ database/group.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    users: [
+
+    ]
+})
+
+module.exports = mongoose.model("group", schema)
